77f73fa7ffd8c75c127936884ab0340b8b82f13b
Dominik Reczek <dreczek@veepee.com>
2021-04-18 17:34:44 +0200

    Init commit

---
+++ TheShop/Source/Home/HomeViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder: HomeViewController) has not been implemented")
+    }
+}
