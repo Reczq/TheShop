97a285491ea8e42298ac846dab9ccbd6ec9c23b8
Dominik Reczek <dreczek@veepee.com>
2021-04-18 17:38:01 +0200

    Handled viewDidLoad in HomeViewController

--- TheShop/Source/Home/HomeViewController.swift
+++ TheShop/Source/Home/HomeViewController.swift
@@ -5,8 +5,12 @@ final class HomeViewController: UIViewController {
     init() {
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder: HomeViewController) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 }
